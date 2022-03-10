1ac9bb5597dde48d41c53cee3d0fb08988da62df
Lawton Nichols <lawtonnichols@gmail.com>
2022-03-09 17:09:41 -0800

    .

---
+++ csci1/lec15/goto.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.shape("turtle")
+print(turtle.pos())
+
+# L
+turtle.up()
+turtle.goto(-200, 100)
+turtle.down()
+turtle.goto(-200, -100)
+turtle.goto(-75, -100)
+
+# space
+turtle.up()
+turtle.goto(-25, -100)
+turtle.down()
+
+# N
+turtle.goto(-25, 100)
+turtle.goto(100, -100)
+turtle.goto(100, 100)
