2f79da05927309dfc54dcf89ed4f3a6ba13749a0
Lawton Nichols <lawtonnichols@gmail.com>
2022-03-16 18:38:37 -0700

    .

---
+++ csci1/lec17/random-squares.py
@@ -0,0 +1,117 @@
+import turtle
+import random
+
+turtle.shape("turtle")
+
+color_to_use = input("Enter the color: ")
+turtle.fillcolor(color_to_use)
+
+#############
+
+starting_x = random.randint(-300, 300)
+starting_y = random.randint(-300, 300)
+square_side_length = random.randint(50, 200)
+
+# go to the random starting point
+turtle.up()
+turtle.goto(starting_x, starting_y)
+turtle.down()
+
+# draw a square there with the correct random size
+turtle.begin_fill()
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.end_fill()
+
+#############
+
+starting_x = random.randint(-300, 300)
+starting_y = random.randint(-300, 300)
+square_side_length = random.randint(50, 200)
+
+# go to the random starting point
+turtle.up()
+turtle.goto(starting_x, starting_y)
+turtle.down()
+
+# draw a square there with the correct random size
+turtle.begin_fill()
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.end_fill()
+
+#############
+
+starting_x = random.randint(-300, 300)
+starting_y = random.randint(-300, 300)
+square_side_length = random.randint(50, 200)
+
+# go to the random starting point
+turtle.up()
+turtle.goto(starting_x, starting_y)
+turtle.down()
+
+# draw a square there with the correct random size
+turtle.begin_fill()
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.end_fill()
+
+#############
+
+starting_x = random.randint(-300, 300)
+starting_y = random.randint(-300, 300)
+square_side_length = random.randint(50, 200)
+
+# go to the random starting point
+turtle.up()
+turtle.goto(starting_x, starting_y)
+turtle.down()
+
+# draw a square there with the correct random size
+turtle.begin_fill()
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.end_fill()
+
+#############
+
+starting_x = random.randint(-300, 300)
+starting_y = random.randint(-300, 300)
+square_side_length = random.randint(50, 200)
+
+# go to the random starting point
+turtle.up()
+turtle.goto(starting_x, starting_y)
+turtle.down()
+
+# draw a square there with the correct random size
+turtle.begin_fill()
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.left(90)
+turtle.forward(square_side_length)
+turtle.end_fill()
---
+++ csci1/lec17/randomness.py
@@ -0,0 +1,4 @@
+import random
+
+N = random.randint(1, 10) # generates a rand # between 1 and 10
+print(N)
